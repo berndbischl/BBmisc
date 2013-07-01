d80c0f3e1a0d838fb23520eb399d16e4c8218477
mllg <michellang@gmail.com>
2013-07-02 01:04:37 +0200

    tweaked convertdfcols

--- skel/R/convertDfCols.R
+++ skel/R/convertDfCols.R
@@ -1,7 +1,7 @@
-#' Converts all character columns in a data.frame to factors
+#' Converts columns in a data frame to characters, factors or numerics.
 #'
 #' @param df [\code{data.frame}]\cr
-#'   Data frame-
+#'   Data frame.
 #' @param chars.as.factor [\code{logical(1)}]\cr
 #'   Should characters be converted to factors?
 #'   Default is \code{FALSE}.
@@ -14,32 +14,29 @@
 #' @export
 #' @return [\code{data.frame}].
 convertDfCols = function(df, chars.as.factor = FALSE, factors.as.char = FALSE, ints.as.num = FALSE) {
-  #FIXME: speed in general can be increased
   checkArg(df, "data.frame")
   checkArg(chars.as.factor, "logical", len=1L, na.ok=FALSE)
   checkArg(factors.as.char, "logical", len=1L, na.ok=FALSE)
   checkArg(ints.as.num, "logical", len=1L, na.ok=FALSE)
-  df2 = df
+  df = x = as.list(df)
+
   if (chars.as.factor) {
-    inds = which(sapply(df, is.character))
-    if (length(inds) > 0) {
-      for (i in inds)
-        df2[, i] = factor(df[, i])
-    }
+    i = vapply(df, is.character, TRUE)
+    if (any(i))
+      x[i] = lapply(x[i], factor)
   }
+
   if (factors.as.char) {
-    inds = which(sapply(df, is.factor))
-    if (length(inds) > 0) {
-      for (i in inds)
-        df2[, i] = as.character(df[, i])
-    }
+    i = vapply(df, is.factor, TRUE)
+    if (any(i))
+      x[i] = lapply(x[i], as.character)
   }
+
   if (ints.as.num) {
-    inds = which(sapply(df, is.integer))
-    if (length(inds) > 0) {
-      for (i in inds)
-        df2[, i] = as.numeric(df[, i])
-    }
+    i = vapply(df, is.integer, TRUE)
+    if (any(i))
+      x[i] = lapply(x[i], as.double)
   }
-  return(df2)
+
+  as.data.frame(x, stringsAsFactors=FALSE)
 }
