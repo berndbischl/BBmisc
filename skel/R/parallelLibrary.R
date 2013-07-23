bdb848481af84cddbdfb85c1d7c87f6289a218b2
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-07-23 15:55:37 +0200

    fix parallelLibrary

--- skel/R/parallelLibrary.R
+++ skel/R/parallelLibrary.R
@@ -1,15 +1,23 @@
 #' Load packages on slaves.
 #'
 #' In case of snowfall mode, uses a combination of
-#' \code{\link[snowfall]{sfClusterEval}} and \code{\link{requitre}}
+#' \code{\link[snowfall]{sfClusterEval}} and \code{\link{require}}
 #' to load the packages. For all other modes, this is not needed and the
 #' function does nothing.
 #'
 #' @param packages [\code{character}]\cr
 #'   Names of packages to load.
+#' @param level [\code{character(1)}]\cr
+#'   The function only loads the packages if the same level is specified in
+#'   \code{\link{parallelStart}} or this argument is \code{NA}.
+#'   See \code{\link{parallelMap}}. 
+#'   Default is \code{NA}.
 #' @return Nothing.
 #' @export
-parallelLibrary = function(packages, level) {
+parallelLibrary = function(packages, level=as.character(NA)) {
+  checkArg(packages, "character", na.ok=FALSE)
+  checkArg(level, "character", len=1L, na.ok=TRUE)
+  
   if (getOption("BBmisc.parallel.mode") == "snowfall" && 
         (is.na(getOption("BBmisc.parallel.level")) || 
            getOption("BBmisc.parallel.level") == level)) {
