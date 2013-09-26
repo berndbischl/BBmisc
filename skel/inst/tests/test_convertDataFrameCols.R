8c6ae70d6bcd65fe8fbadbbbcb1d0e2a685d6d18
Bernd Bischl <bernd_bischl@gmx.net>
2013-09-27 00:51:33 +0200

    cleanup

--- skel/inst/tests/test_convertDataFrameCols.R
+++ skel/inst/tests/test_convertDataFrameCols.R
@@ -1,8 +1,8 @@
-context("convertDfCols")
+context("convertDataFrameCols")
 
-test_that("convertDfCols", {
+test_that("convertDataFrameCols", {
   df = data.frame(x=1:2, y=c("a", "b"), z=factor(c("x", "y")), stringsAsFactors=FALSE)
-  df2 = convertDfCols(df, chars.as.factor=TRUE)
+  df2 = convertDataFrameCols(df, chars.as.factor=TRUE)
   expect_true(is.numeric(df2$x))
   expect_true(is.factor(df2$y))
   expect_true(is.factor(df2$z))
@@ -10,7 +10,7 @@ test_that("convertDfCols", {
   expect_equal(df$y, as.character(df2$y))
   expect_equal(df$z, df2$z)
 
-  df2 = convertDfCols(df, factors.as.char=TRUE)
+  df2 = convertDataFrameCols(df, factors.as.char=TRUE)
   expect_true(is.numeric(df2$x))
   expect_true(is.character(df2$y))
   expect_true(is.character(df2$z))
@@ -18,7 +18,7 @@ test_that("convertDfCols", {
   expect_equal(df$y, df2$y)
   expect_equal(as.character(df$z), df2$z)
   
-  df2 = convertDfCols(df, ints.as.num=TRUE)
+  df2 = convertDataFrameCols(df, ints.as.num=TRUE)
   expect_true(is.double(df2$x))
   expect_true(is.character(df2$y))
   expect_true(is.factor(df2$z))
@@ -26,7 +26,7 @@ test_that("convertDfCols", {
   expect_equal(df$y, df2$y)
   expect_equal(df$z, df2$z)
 
-  df2 = convertDfCols(df, chars.as.factor=TRUE, factors.as.char=TRUE, ints.as.num=TRUE)
+  df2 = convertDataFrameCols(df, chars.as.factor=TRUE, factors.as.char=TRUE, ints.as.num=TRUE)
   expect_true(is.double(df2$x))
   expect_true(is.factor(df2$y))
   expect_true(is.character(df2$z))
