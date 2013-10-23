9d17adaa67d36aab759f2006ebe032689c01be47
Bernd Bischl <bernd_bischl@gmx.net>
2013-10-23 03:17:44 +0200

    increase version to 1.4 and some code cleanup for toRangeStr

--- R/toRangeStr.R
+++ R/toRangeStr.R
@@ -1,13 +1,14 @@
 #' Convert a numerical vector into a range string.
 #'
-#' @param x [\code{integer()}]\cr
+#' @param x [\code{integer}]\cr
 #'   Vector to convert into a range string.
 #' @param range.sep [\code{character(1)}]\cr
 #'   Separator between the first and last element of a range of consecutive
 #'   elements in \code{x}.
+#'   Default is \dQuote{ - }.
 #' @param block.sep [\code{character(1)}]\cr
 #'   Separator between non consecutive elements of \code{x} or ranges.
-#'
+#'   Default is \dQuote{, }.
 #' @return [\code{character(1)}]
 #' @examples
 #' x <- sample(1:10, 7)
@@ -20,27 +21,26 @@ toRangeStr = function(x, range.sep=" - ", block.sep=", ") {
   checkArg(block.sep, "character", len=1, na.ok=FALSE)
   
   findRange = function(x) {
-    end_index = max(which(x == x[1] + 0:(length(x)-1)))
+    end_index = max(which(x == x[1L] + 0:(length(x)-1L)))
     1:end_index
   }
-  sorted_x = sort(unique(x))
+  sorted.x = sort(unique(x))
   res = c()
-  while(length(sorted_x) > 0) {
-    ## Find the longest head of sorted_x that has consecutive elements. 
-    rng = findRange(sorted_x)
-    ## Extract the leading range
-    head = sorted_x[rng]
-    ## Trim leading range.
-    sorted_x = sorted_x[-rng]    
-    res = if (length(head) == 1) {
+  while(length(sorted.x) > 0L) {
+    # Find the longest head of sorted_x that has consecutive elements. 
+    rng = findRange(sorted.x)
+    # Extract the leading range
+    head = sorted.x[rng]
+    # Trim leading range.
+    sorted.x = sorted.x[-rng]    
+    res = if (length(head) == 1L) {
       c(res, format(head))
     } else {
-      ## Collapse range into "min(head) - max(head)" string.
-      c(res, paste(c(head[1], head[length(head)]), collapse=range.sep))
+      # Collapse range into "min(head) - max(head)" string.
+      c(res, collapse(c(head[1L], head[length(head)]), range.sep))
     }
   }
-  ## Collapse elements/ranges with blockSep
-  paste(res, collapse=block.sep)
+  collapse(res, block.sep)
 }
 
 
--- inst/tests/test_toRangeStr.R
+++ inst/tests/test_toRangeStr.R
@@ -1,6 +1,6 @@
 context("toRangeStr")
 
-expect_range <- function(x, str, ...) {
+expect_range = function(x, str, ...) {
   expect_equal(toRangeStr(x, ...), str)
   expect_equal(toRangeStr(sample(x), ...), str)
   expect_equal(toRangeStr(sample(c(x, x)), ...), str)
@@ -8,23 +8,23 @@ expect_range <- function(x, str, ...) {
 }
 
 test_that("continuous ranges", {
-  x <- c(1, 2, 3, 4, 5, 6)
+  x = c(1, 2, 3, 4, 5, 6)
   expect_range(x, "1 - 6")
   expect_range(x, "1_6", range.sep="_")
 })
 
 test_that("single number", {
-  x <- 1
+  x = 1
   expect_range(x, "1")
 })
 
 test_that("negative numbers", {
-  x <- -2:4
+  x = -2:4
   expect_range(x, "-2 - 4")
 })
 
 test_that("noncontinuous ranges", {
-  x <- c(-5, -4, -2, 0, 2, 3, 4, 7)
+  x = c(-5, -4, -2, 0, 2, 3, 4, 7)
   expect_range(x, "-5 - -4, -2, 0, 2 - 4, 7")
 })
 
