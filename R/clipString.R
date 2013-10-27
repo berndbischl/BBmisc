728172901fd1c9787b2e50b948af4788edee1850
mllg <michellang@gmail.com>
2013-10-27 22:15:55 +0100

    vectorized clipString

--- R/clipString.R
+++ R/clipString.R
@@ -1,7 +1,7 @@
-#' Shortens a string to a given length.
+#' Shortens strings to a given length.
 #' 
-#' @param x [\code{character(1)}]\cr
-#'   String.
+#' @param x [\code{character}]\cr
+#'   Vector of strings.
 #' @param len [\code{integer(1)}]\cr
 #'   Absolute length the string should be clipped to, including \code{tail}.
 #'   Note that you cannot clip to a shorter length than \code{tail}.
@@ -16,13 +16,12 @@
 #FIXME what abou NAs.
 # careful: in BJ this functuion was used, the code there had NA->NA!
 clipString = function(x, len, tail="...") {
-  checkArg(x, "character", len=1L, na.ok=FALSE)
+  checkArg(x, "character", na.ok=FALSE)
   len = convertInteger(len)
   checkArg(tail, "character", len=1L, na.ok=FALSE)
   checkArg(len, "integer", len=1L, na.ok=FALSE, lower=nchar(tail))
   #if (is.na(x))
   #  return(NA_character_)
-  if (nchar(x) > len)
-    return(paste(substr(x, 1L, len - nchar(tail)), tail, sep=""))
-  return(x)
+  ind = nchar(x) > len
+  replace(x, ind, paste(substr(x[ind], 1L, len - nchar(tail)), tail, sep=""))
 }
