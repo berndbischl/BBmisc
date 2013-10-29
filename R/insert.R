df6fda75bc65cbbddf954bf3d75301428030dbda
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-10-29 01:00:17 +0100

    insert now also works for normal vectors

--- R/insert.R
+++ R/insert.R
@@ -1,16 +1,16 @@
-#' Insert elements from one list into another list.
+#' Insert elements from one list/vector into another list/vector.
 #' 
-#' Inserts elements from list \code{x2} into list \code{x1} by name,
+#' Inserts elements from \code{xs2} into \code{xs1} by name,
 #' overwriting elements of equal names.
 #' 
 #' @param xs1 [\code{list}]\cr
-#'   First list. 
+#'   First list/vector. 
 #' @param xs2 [\code{list}]\cr
-#'   Second list. Must be fully and uniquely named.
+#'   Second vector/list. Must be fully and uniquely named.
 #' @param elements [\code{character}]\cr
 #'   Elements from \code{xs2} to insert into \code{xs1}.
 #'   Default is all.
-#' @return [\code{list}]. \code{x1} with replaced elements from \code{x2}.
+#' @return \code{x1} with replaced elements from \code{x2}.
 #' @export
 #' @examples
 #' xs1 <- list(a=1, b=2)
@@ -18,11 +18,13 @@
 #' insert(xs1, xs2)
 #' insert(xs1, xs2, elements="c")
 insert = function(xs1, xs2, elements) {
-  if (missing(elements)) {
-    xs1[names(xs2)] = xs2
-  } else {
-    elements = intersect(elements, names(xs2))
-    xs1[elements] = xs2[elements]
+  if (length(xs2) > 0L) {
+    if (missing(elements)) {
+      xs1[names(xs2)] = xs2
+    } else {
+      elements = intersect(elements, names(xs2))
+      xs1[elements] = xs2[elements]
+    }
   }
   return(xs1)
 }
