1b42d204fa8552fbd022920a8087af51c47b4bfa
Bernd Bischl <bernd_bischl@gmx.net>
2013-10-30 13:19:40 +0100

    chunk: better distribution of chunks sizes, fixes mlr issue

--- R/chunk.R
+++ R/chunk.R
@@ -1,15 +1,20 @@
 #' Chunk elements of vectors into blocks of nearly equal size.
 #'
+#' In case of shuffling and vectors that cannot be chunked evenly,
+#' it is chosen randomly which levels / chunks will receive 1 element less.
+#' If you do not shuffle, always the last chunks will receive 1 element less.
+#'
 #' @param x [\code{vector} | \code{list}]\cr
-#'  Vector or list to chunk into blocks of nearly equal size.
+#'   Vector or list to chunk.
 #' @param chunk.size [\code{integer(1)}]\cr
-#'   Preferred number of jobs in each chunk.
-#'   Can not be used in combination with \code{n.chunks}
+#'   Requested number of elements in each chunk.
+#'   Cannot be used in combination with \code{n.chunks}.
+#'   If \code{x} cannot be evenly chunked, some chunks will have 1 element less. 
 #' @param n.chunks [\code{integer(1)}]\cr
-#'   Preferred number of chunks.
+#'   Requested number of chunks.
 #'   Can not be used in combination with \code{chunks.size}
 #' @param shuffle [\code{logical(1)}]\cr
-#'   Shuffle the vector or list?
+#'   Shuffle \code{x}?
 #'   Default is \code{FALSE}.
 #' @return [unnamed \code{list}] of chunks.
 #' @export
@@ -19,52 +24,27 @@
 #' chunk(xs, n.chunks=2)
 #' chunk(xs, n.chunks=2, shuffle=TRUE)
 chunk = function(x, chunk.size, n.chunks, shuffle=FALSE) {
+  checkArg(x, "vector")
+  nx = length(x)
   if (!xor(missing(chunk.size), missing(n.chunks)))
     stop("You must provide either chunk.size (x)or n.chunks")
-
-  n = length(x)
-  if (!missing(n.chunks)) {
-    n.chunks = convertInteger(n.chunks)
-    checkArg(n.chunks, "integer", len=1L, lower=1L, na.ok=FALSE)
-    ch = sort(seq(0L, n - 1L) %% n.chunks + 1L)
-  } else { # !missing(chunk.size)
+  if (missing(n.chunks)) {
     chunk.size = convertInteger(chunk.size)
     checkArg(chunk.size, "integer", len=1L, lower=1L, na.ok=FALSE)
-    n.chunks = n %/% chunk.size + (n %% chunk.size > 0L)
-    ch = head(rep(seq_len(n.chunks), each=chunk.size), n)
+    n.chunks = nx %/% chunk.size + (nx %% chunk.size > 0L)
+  } else {
+    n.chunks = convertInteger(n.chunks)
+    checkArg(n.chunks, "integer", len=1L, lower=1L, na.ok=FALSE)
   }
+  checkArg(shuffle, "logical", len=1L, na.ok=FALSE)
 
-  if(shuffle)
-    x = sample(x)
+  n.chunks = min(n.chunks, nx)
+  if (shuffle) {
+    # take care that we randomly choose levels which potentially have less elems
+    ch = sample(c(seq(0L, (nx %/% n.chunks) * n.chunks - 1L) %% n.chunks + 1L,
+      sample(n.chunks, nx %% n.chunks)))
+  } else {
+    ch = sort(seq(0L, nx - 1L) %% n.chunks) + 1
+  }
   unname(split(x, ch))
 }
-
-# this is from issue tracker of mlr
-# works slightly different in the way groups sizes are balanced, 
-# e.g. (3 2 2) instead of (3 3 1)
-# important for stratified cv!
-# FIXME re-read, adapt tests and replace?
-# chunk = function(x, chunk.size, n.chunks, shuffle=FALSE) {
-#   nx = length(x)
-
-#   if (!xor(missing(chunk.size), missing(n.chunks)))
-#     stop("You must provide either chunk.size (x)or n.chunks")
-#   if (missing(n.chunks)) {
-#     chunk.size = convertInteger(chunk.size)
-#     checkArg(chunk.size, "integer", len=1L, lower=1L, na.ok=FALSE)
-#     n.chunks = nx %/% chunk.size + (nx %% chunk.size > 0L)
-#   } else {
-#     n.chunks = convertInteger(n.chunks)
-#     checkArg(n.chunks, "integer", len=1L, lower=1L, na.ok=FALSE)
-#   }
-#   checkArg(shuffle, "logical", len=1L, na.ok=FALSE)
-
-#   n.chunks = min(n.chunks, nx)
-#   if (shuffle) {
-#     ch = sample(c(seq(0L, (nx %/% n.chunks) * n.chunks - 1L) %% n.chunks + 1L,
-#                   sample(n.chunks, nx %% n.chunks)))
-#   } else {
-#     ch = sort(seq(0L, nx - 1L) %% n.chunks + 1L)
-#   }
-#   unname(split(x, ch))
-# }
