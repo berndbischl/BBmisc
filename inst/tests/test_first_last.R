49f9502f3f89518a1781cb94e74a64705c933133
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-11-16 16:16:32 +0100

    clean up michels new funnctions....

---
+++ inst/tests/test_first_last.R
@@ -0,0 +1,17 @@
+context("first / last")
+
+test_that("first / last", {
+  x = c(FALSE, TRUE, FALSE, TRUE)
+  expect_equal(first(x), 2L)
+  expect_equal(last(x), 4L)
+
+  x = c(FALSE, TRUE, NA, TRUE)
+  expect_equal(first(x), 2L)
+  expect_equal(last(x), 4L)
+  expect_equal(first(x, na.omit=FALSE), 2L)
+  expect_equal(last(x, na.omit=FALSE), 4L)
+
+  x = c(NA, TRUE, NA, TRUE, NA)
+  expect_equal(first(x, na.omit=FALSE), NA_integer_)
+  expect_equal(last(x, na.omit=FALSE), NA_integer_)
+})
