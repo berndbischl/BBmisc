9ad06379746108092d54ae26a92ab909eedbbd90
Michel Lang <michellang@gmail.com>
2013-12-05 12:57:11 +0000

    added curriedCall

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+#' @importFrom functional CurryL
 .onLoad = function(libname, pkgname) {
   options(BBmisc.ProgressBar.style = getOption("BBmisc.ProgressBar.style", "text"))
   options(BBmisc.ProgressBar.width = getOption("BBmisc.ProgressBar.width", getOption("width")))
