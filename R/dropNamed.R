49832c7dc6b0b571bf39ed8356315d0501fae00e
Bernd Bischl <bernd_bischl@gmx.net>
2013-12-07 02:36:24 +0100

    add unit test for dropNamed

--- R/dropNamed.R
+++ R/dropNamed.R
@@ -1,15 +1,14 @@
-#FIXME: unit test missing
-
 #' Drop named elements of an object.
 #'
 #' @param x [any]\cr
 #'   Object to drop named elements from.
-#'   For matrix and data frames this function delegates to drop
-#'   using the second dimension (columns).
-#'   The normal index operator \dQuote{[} is applied otherwise..
+#'   For a matrix or a data frames this function drops named columns via
+#'   the second argument of the binary index operator \code{[,]}.
+#'   Otherwise, the unary index operator \code{[]} is used for dropping.
 #' @param drop [\code{character}]\cr
-#'   Names to drop.
-#' @return Subset of object of same type as \code{x}.
+#'   Names of elements to drop.
+#' @return Subset of object of same type as \code{x}. The object is not simplified,
+#'   i.e, no dimensions are dropped as \code{[,,drop = FALSE]} is used.
 #' @export
 dropNamed = function(x, drop) {
   checkArg(drop, "character", na.ok=FALSE)
