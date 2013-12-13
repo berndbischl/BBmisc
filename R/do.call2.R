b42cb19f83af2579e6654c1f917ce4842b05924f
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-12-13 14:32:53 +0100

    clean up

--- R/do.call2.R
+++ R/do.call2.R
@@ -1,3 +1,11 @@
+#FIXME:
+
+# a) REALLY test this... 
+# b) Proof that this increases performance with at least one example
+
+
+#FIXME: bug with anonymous function?
+
 #' Execute a function call
 #'
 #' This function is supposed to be a replacement for \code{\link[base]{do.call}} in situations
@@ -5,12 +13,13 @@
 #' inflicting a copy.
 #'
 #' @param fun [\code{function}]\cr
-#'  Function to call.
+#'   Function to call.
 #' @param ... [any]\cr
-#'  Arguments to \code{fun}. Best practice is to specify them in a \code{key=value} syntax.
+#'   Arguments to \code{fun}. Best practice is to specify them in a \code{key=value} syntax.
 #' @param .args [\code{list}]\cr
-#'  Arguments to \code{fun} as a (named) list. Will be passed after arguments in \code{...}.
-#'  Default is \code{list()}.
+#'   Arguments to \code{fun} as a (named) list. Will be passed after arguments in \code{...}.
+#'   Default is \code{list()}.
+#FIXME: return?
 do.call2 = function(fun, ..., .args=list()) {
   checkArg(.args, "list")
   if (is.function(fun))
