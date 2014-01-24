a94c52e4165eb292d40f81c1c9575591bfffd591
Michel <michellang@gmail.com>
2014-01-24 15:43:14 +0100

    re-added deprecated functions otherwise we break cran packages

---
+++ R/deprecated.R
@@ -0,0 +1,17 @@
+#' DEPRECATED
+#'
+#' Use \code{\link{convertDataFrameCols}} instead.
+#' @keywords internal
+#' @export
+convertDfCols = function(...) {
+  convertDataFrameCols(...)
+}
+
+#' DEPRECATED
+#'
+#' Use \code{\link{listToShortString}} instead.
+#' @keywords internal
+#' @export
+listToShortString = function(...) {
+  convertToShortString(...)
+}
