4604e8625b0704165e02d3fc8693b12afee5f2bf
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-01-31 20:04:28 +0100

    add convertListOfRowsToDataFrame again

--- R/convertListOfRowsToDataFrame.R
+++ R/convertListOfRowsToDataFrame.R
@@ -8,8 +8,6 @@
 #' @param strings.as.factors [\code{logical(1)}]\cr
 #'   Convert character columns to factors?
 #'   Default is \code{default.stringsAsFactors()}.
-#' @param rows [\code{list}]\cr
-#'   List of list of rows, where each inner list is of the same structure.
 #' @param row.names [\code{character(nrow)} | \code{NULL}]\cr
 #'   Row names.
 #'   Default is \code{NULL}.
@@ -20,23 +18,22 @@
 #' @export
 #' @examples
 #' convertListofRowsToDataFrame(list(list(x=1, y="a"), list(x=2, y="b")))
-#FIXME reread check and test
-convertListofRowsToDataFrame = function(rows, force.names=FALSE, strings.as.factors = default.stringsAsFactors(),
-                                        row.names=NULL, col.names) {
-  
+convertListofRowsToDataFrame = function(rows, force.names=FALSE,
+  strings.as.factors = default.stringsAsFactors(), row.names=NULL, col.names) {
+
   n = length(rows)
   if (n == 0L)
-    return(as.data.frame(matrix(nrow = 0L, ncol = 0L)))
-  
+    return(as.data.frame(matrix(nrow=0L, ncol=0L)))
+
   if (force.names) {
     rows = lapply(rows, function(r) setNames(r, make.names(names2(r, ""), unique=TRUE)))
   }
-  
+
   cols = unique(unlist(lapply(rows, names)))
-  
+
   if (length(cols) == 0L)
-    return(as.data.frame(matrix(nrow = n, ncol = 0L)))
-  
+    return(as.data.frame(matrix(nrow=n, ncol=0L)))
+
   res = namedList(cols)
   for(col in cols) {
     tmp = lapply(rows, function(r) r[[col]])
