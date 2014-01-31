fed900512349560b3e1a0691eff21b9eea705a11
Jakob Richter <code@jakob-r.de>
2014-01-31 15:09:24 +0100

    update renaming

--- inst/tests/test_btwn.R
+++ inst/tests/test_btwn.R
@@ -1,10 +1,10 @@
-context("within")
+context("btwn")
 
-test_that("within", {
+test_that("btwn", {
   y = c(-1L,5L,Inf)
-  expect_equal(1L:3L %within% y, c(TRUE, TRUE, TRUE))
-  expect_equal(-2L:-1L %within% y, c(FALSE,TRUE))
+  expect_equal(1L:3L %btwn% y, c(TRUE, TRUE, TRUE))
+  expect_equal(-2L:-1L %btwn% y, c(FALSE,TRUE))
   y = 5L
-  expect_equal(5L %within% y, TRUE)
-  expect_equal(1L %within% y, FALSE)
+  expect_equal(5L %btwn% y, TRUE)
+  expect_equal(1L %btwn% y, FALSE)
 })
