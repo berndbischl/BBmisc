62b0672848c1a8281d5886c4475f7eb3e85b8785
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-01-31 18:54:54 +0100

    clean up

--- R/btwn.R
+++ R/btwn.R
@@ -1,9 +1,10 @@
-#' Check if value is in between a range.
+#' Check if some values are covered by the range of the values in a second vector.
 #'
-#' @param x [\code{numeric(m)}]\cr
+#' @param x [\code{numeric(n)}]\cr
 #'   Value(s) that should be within the range of \code{y}.
-#' @param y [\code{numeric(n)}]\cr
-#'   Numeric vector which defines the range
+#' @param y [\code{numeric}]\cr
+#'   Numeric vector which defines the range.
+#' @return [\code{logical(n)}]. For each value in \code{x}: Is it in the range of \code{y}?
 #' @usage x \%btwn\% y
 #' @rdname btwn
 #' @examples
@@ -12,7 +13,7 @@
 #' x %btwn% y
 #' @export
 `%btwn%` = function(x, y) {
-  t = range(y)
-  x <= t[2] & x >= t[1]
+  r = range(y)
+  x <= r[2] & x >= r[1]
 }
 
