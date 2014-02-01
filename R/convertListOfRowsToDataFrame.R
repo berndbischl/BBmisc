94a2e15367408fde36fbefdfa91b33772553ffbd
Bernd Bischl <bernd_bischl@gmx.net>
2014-02-01 18:15:20 +0100

    improve convertListOfRowsToDataFrame

--- R/convertListOfRowsToDataFrame.R
+++ R/convertListOfRowsToDataFrame.R
@@ -1,43 +1,64 @@
-#' Convert a list of row-lists of equal structure to a data.frame.
+#' Convert a list of row-vectors of equal structure to a data.frame.
 #'
 #' @param rows [\code{list}]\cr
-#'   List of list of rows, where each inner list is of the same structure.
-#' @param force.names [\code{logical(1)}]\cr
-#'   Convert character columns to factors?
-#'   Default is \code{default.stringsAsFactors()}.
+#'   List of rows. Each row is a list or vector of the same structure.
+#'   That means all rows must have the same length and all corresponding elements must have the 
+#'   same class.
 #' @param strings.as.factors [\code{logical(1)}]\cr
 #'   Convert character columns to factors?
 #'   Default is \code{default.stringsAsFactors()}.
 #' @param row.names [\code{character(nrow)} | \code{NULL}]\cr
-#'   Row names.
-#'   Default is \code{NULL}.
+#'   Row names for result.
+#'   \code{NULL} implies no row names are set.
+#'   By default the names of the list \code{rows} are taken.
 #' @param col.names [\code{character(ncol)}]\cr
 #'   Column names.
-#'   Default is \dQuote{V1}, \dQuote{V2}, and so on.
+#'   Default the are the names of the elements of the first row, or, 
+#'   if this row has no element names, \dQuote{V1}, \dQuote{V2}, 
+#'   and so on, is used.
 #' @return [\code{data.frame}].
 #' @export
 #' @examples
-#' convertListofRowsToDataFrame(list(list(x=1, y="a"), list(x=2, y="b")))
-convertListofRowsToDataFrame = function(rows, force.names=FALSE,
-  strings.as.factors = default.stringsAsFactors(), row.names=NULL, col.names) {
+#' convertListOfRowsToDataFrame(list(list(x=1, y="a"), list(x=2, y="b")))
+convertListOfRowsToDataFrame = function(rows, strings.as.factors = default.stringsAsFactors(), 
+  row.names, col.names) {
 
-  n = length(rows)
-  if (n == 0L)
+  checkArg(rows, "list")
+  checkListElementClass(rows, "vector")
+  nrows = length(rows)
+  if (nrows == 0L)
     return(as.data.frame(matrix(nrow=0L, ncol=0L)))
-
-  if (force.names) {
-    rows = lapply(rows, function(r) setNames(r, make.names(names2(r, ""), unique=TRUE)))
+  checkArg(strings.as.factors, "logical", len=1L, na.ok=FALSE)
+  if (missing(row.names)) {
+    row.names = names(rows)
+  } else {
+    checkArg(row.names, "character", len=nrows, na.ok=FALSE)
+  }
+  ncols = length(rows[[1L]])
+  if (ncols == 0L)
+    return(as.data.frame(matrix(nrow=nrows, ncol=0L)))
+  if (missing(col.names)) {
+    if (isProperlyNamed(rows[[1]]))
+      col.names = names(rows[[1]])
+    else
+      col.names = paste0("V", 1:ncols)
+  } else {
+    checkArg(col.names, "character", len=ncols, na.ok=FALSE)
   }
 
-  cols = unique(unlist(lapply(rows, names)))
+  # FIXME: check that rows have the same "structure" and length
+  
+  # force all names
+  rows = lapply(rows, function(r) setNames(r, col.names))
 
-  if (length(cols) == 0L)
-    return(as.data.frame(matrix(nrow=n, ncol=0L)))
-
-  res = namedList(cols)
-  for(col in cols) {
-    tmp = lapply(rows, function(r) r[[col]])
-    res[[col]] = simplify2array(replace(tmp, vapply(tmp, is.null, logical(1L)), NA))
+  res = namedList(col.names)
+  for(cn in col.names) {
+    # pick out column element from each row
+    col = lapply(rows, function(r) r[[cn]])
+    # recode NULLs with NAs
+    res[[cn]] = simplify2array(replace(col, vapply(col, is.null, logical(1L)), NA))
   }
-  as.data.frame(res, stringsAsFactors = strings.as.factors)
+  res = as.data.frame(res, stringsAsFactors = strings.as.factors)
+  rownames(res) = row.names
+  return(res)
 }
--- inst/tests/test_convertListOfRowsToDataFrame.R
+++ inst/tests/test_convertListOfRowsToDataFrame.R
@@ -1,7 +1,16 @@
 context("convertListOfRowstoDataFrame")
 
 test_that("convertListOfRowstoDataFrame", {
-  df1 = convertListofRowsToDataFrame(list(list(x=1, y="a"), list(x=2, y="b")), strings.as.factors=FALSE)
+  df1 = convertListOfRowsToDataFrame(list(list(x=1, y="a"), list(x=2, y="b")), strings.as.factors=FALSE)
   df2 = data.frame(x=1:2, y=c("a", "b"), stringsAsFactors=FALSE)
+  expect_equal(df1, df2)
+
+  df1 = convertListOfRowsToDataFrame(list(c(x="1", y="a"), list(x="2", y="b")), strings.as.factors=FALSE)
+  df2 = data.frame(x=c("1", "2"), y=c("a", "b"), stringsAsFactors=FALSE)
+  expect_equal(df1, df2)
+
+  df1 = convertListOfRowsToDataFrame(list(c("1", "a"), list("2", "b")), strings.as.factors=FALSE, 
+    col.names=c("x", "y"))
+  df2 = data.frame(x=c("1", "2"), y=c("a", "b"), stringsAsFactors=FALSE)
   expect_equal(df1, df2)
 })
