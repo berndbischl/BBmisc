ffd6bb5104964da44a32d69345c74821a7e38a4e
Michel <michellang@gmail.com>
2014-03-03 14:55:46 +0100

    dapply fixes

--- R/dapply.R
+++ R/dapply.R
@@ -9,16 +9,10 @@
 #' @param col.names [\code{character(1)}]\cr
 #'   Column names for result.
 #'   Default are the names of \code{x}.
-#' @param quick [\code{logical(1)}]\cr
-#'   ??????
-#'   Default is \code{TRUE}.
 #' @export
 #' @return [\code{data.frame}].
-dapply = function(x, fun, ..., col.names, quick=TRUE) {
-  # FIXME: finish this
-  # maybe better not check x type to be really general?
+dapply = function(x, fun, ..., col.names) {
   checkArg(fun, "function")
-  checkArg(quick, "logical", len=1L, na.ok=FALSE)
 
   x = lapply(x, fun, ...)
 
@@ -32,19 +26,15 @@ dapply = function(x, fun, ..., col.names, quick=TRUE) {
     names(x) = col.names
   }
 
-  if (quick) {
-    n = unique(viapply(x, length))
-    if (length(n) > 1L) {
-      max.n = max(n)
-      if (any(max.n %% n))
-        stop("Arguments imply differing number of rows: ", collapse(n, ", "))
-      x = lapply(x, rep_len, length.out=max.n)
-      n = max.n
-    }
-    attr(x, "row.names") = seq_len(n)
-    attr(x, "class") = "data.frame"
-  } else {
-    x = data.frame(x, stringsAsFactors=FALSE)
+  n = unique(viapply(x, length))
+  if (length(n) > 1L) {
+    max.n = max(n)
+    if (any(max.n %% n))
+      stop("Arguments imply differing number of rows: ", collapse(n, ", "))
+    x = lapply(x, rep_len, length.out=max.n)
+    n = max.n
   }
+  attr(x, "row.names") = seq_len(n)
+  attr(x, "class") = "data.frame"
   return(x)
 }
--- inst/tests/test_dapply.R
+++ inst/tests/test_dapply.R
@@ -10,6 +10,11 @@ test_that("dapply", {
   expect_is(d, "data.frame")
   expect_equal(dim(d), c(1L, 2L))
   expect_equal(colnames(d), c("a", "b"))
+
+  d1 = dapply(iris, computeMode)
+  d2 = dapply(iris, computeMode, col.names=letters[1:5])
+  expect_equal(dim(d1), c(1L, 5L))
+  expect_equal(dim(d2), c(1L, 5L))
+  expect_equal(names(d1), names(iris))
+  expect_equal(names(d2), letters[1:5])
 })
-
-
