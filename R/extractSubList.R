66357ac57ba67c3f177b087c254989537f6306a8
Bernd Bischl <bernd_bischl@gmx.net>
2014-03-12 23:05:33 +0100

    extractSubList: new arg 'matrix'

--- R/extractSubList.R
+++ R/extractSubList.R
@@ -1,7 +1,7 @@
 #' Extracts a named element from a list of lists.
-#' 
+#'
 #' @param xs [\code{list}]\cr
-#'   A list of named lists. 
+#'   A list of named lists.
 #' @param element [\code{character(1)}]\cr
 #'   Name of element to extract from the list elements of \code{xs}.
 #' @param element.value [any]\cr
@@ -10,26 +10,42 @@
 #'   If \code{TRUE} \code{\link{sapply}} is used, otherwise \code{\link{lapply}}.
 #'   Default is \code{TRUE}.
 #' @param use.names [\code{logical(1)}]\cr
-#'   If \code{TRUE} and \code{xs} is named, the result is named as \code{xs}, 
+#'   If \code{TRUE} and \code{xs} is named, the result is named as \code{xs},
 #'   otherwise the result is unnamed.
 #'   Default is \code{TRUE}.
-#' @return [\code{list} or simplified vector].
+#' @param matrix [\code{character(1)}]\cr
+#'   If \dQuote{cols}, \code{\link{asMatrixCols}} is called on the result,
+#'   if \dQuote{rows}, \code{\link{asMatrixRows}} is called on the result.
+#'   \code{simplify = TRUE} is not used in this case.
+#'   Default is not to do that.
+#' @return [\code{list} | simplified \code{vector} | \code{matrix}]. See above.
 #' @export
 #' @examples
 #' xs = list(list(a=1, b=2), list(a=5, b=7))
 #' extractSubList(xs, "a")
 #' extractSubList(xs, "a", simplify=FALSE)
-extractSubList = function(xs, element, element.value, simplify=TRUE, use.names=TRUE) {
-  if (!missing(element.value))  
+extractSubList = function(xs, element, element.value, simplify=TRUE, use.names=TRUE, matrix) {
+  checkArg(xs, "list")
+  checkArg(simplify, "logical", len = 1L, na.ok = FALSE)
+  checkArg(use.names, "logical", len = 1L, na.ok = FALSE)
+  if (!missing(element.value))
     ys = vapply(xs, function(x) x[[element]], FUN.VALUE=element.value)
-  else if (simplify)
+  else if (simplify && missing(matrix))
     ys = sapply(xs, function(x) x[[element]], USE.NAMES=use.names)
-  else 
+  else
     ys = lapply(xs, function(x) x[[element]])
   ns = names(xs)
   if (use.names && !is.null(ns))
-    names(ys) = ns 
+    names(ys) = ns
   else
     names(ys) = NULL
+
+  if (!missing(matrix)) {
+    checkArg(matrix, choices = c("cols", "rows"))
+    ys = if (matrix == "cols")
+      asMatrixCols(ys)
+    else
+      asMatrixRows(ys)
+  }
   return(ys)
 }
--- inst/tests/test_extractSubList.R
+++ inst/tests/test_extractSubList.R
@@ -12,21 +12,27 @@ test_that("extractSubList", {
   expect_equal(extractSubList(xs, "x", use.names=FALSE), c(1, 2))
   expect_equal(extractSubList(xs, "y", use.names=FALSE), c("foo", "bar"))
   expect_equal(extractSubList(xs, "z", use.names=FALSE), list(matrix(1,1,1), matrix(2,2,2)))
-  
+
   expect_equal(extractSubList(list(), "x"), list())
-  
+
   expect_equal(extractSubList(list(), "x", element.value=numeric(1)), numeric(0))
   expect_equal(extractSubList(list(), "y", element.value=character(1)), character(0))
-  
+
   expect_equal(extractSubList(xs, "x", element.value=numeric(1)), c(a=1, b=2))
   expect_equal(extractSubList(xs, "y", element.value=character(1)), c(a="foo", b="bar"))
-  
+
   xs = list(
     list(x=1, y="foo", z=matrix(1,1,1)),
     list(x=2L, y="bar", z=matrix(2,2,2))
   )
   expect_equal(extractSubList(xs, "y", use.names=TRUE), c("foo", "bar"))
   expect_equal(extractSubList(xs, "y", use.names=FALSE), c("foo", "bar"))
+
+  expect_equal(
+    extractSubList(list(list(a = 1:2), list(a = 3:4)), "a", matrix = "rows"),
+    matrix(1:4, nrow = 2L, ncol = 2L, byrow = TRUE)
+  )
+
 })
 
 
