e55b881c2464980d751a19b44c914224af1f195e
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-13 17:56:11 +0100

    improve convertRowsToList a bit

--- R/convertRowsToList.R
+++ R/convertRowsToList.R
@@ -1,7 +1,7 @@
 #' Convert rows of data.frame or matrix to lists.
 #'
-#' For each row, one list is constructed, each entry of
-#' the row becomes a separate list element.
+#' For each row, one list/vector is constructed, each entry of
+#' the row becomes a list/vector element.
 #'
 #' @param x [\code{matrix} | \code{data.frame}]\cr
 #'   Object to convert.
@@ -9,20 +9,28 @@
 #'   Name elements of lists with column names of \code{x}.
 #'   Default is \code{FALSE}.
 #' @param factors.as.char [\code{logical(1)}]\cr
-#'   If \code{x} is a data.frame, convert factor columns to 
+#'   If \code{x} is a data.frame, convert factor columns to
 #'   string elements in the resulting lists?
 #'   Default is \code{TRUE}.
-#' @return [\code{list} of lists].
-#' @export 
-convertRowsToList = function(x, use.names=FALSE, factors.as.char=TRUE) {
+#' @param as.vector [\code{logical(1)}]\cr
+#'   If \code{x} is a matrix, store rows as vectors in the resulting list - or otherwise as lists?
+#'   Default is \code{TRUE}.
+#' @return [\code{list} of lists or vectors].
+#' @export
+convertRowsToList = function(x, use.names = FALSE, factors.as.char = TRUE, as.vector = TRUE) {
   checkArg(x, c("matrix", "data.frame"))
-  checkArg(use.names, "logical", len=1L, na.ok=FALSE) 
+  checkArg(use.names, "logical", len = 1L, na.ok = FALSE)
+  checkArg(factors.as.char, "logical", len = 1L, na.ok = FALSE)
+  checkArg(as.vector, "logical", len = 1L, na.ok = FALSE)
   ns = if (use.names) colnames(x) else NULL
   if (is.matrix(x)) {
-    apply(x, 1, function(row) setNames(as.list(row), ns))
+    if (as.vector)
+      lapply(seq_row(x), function(i) setNames(x[i, ], ns))
+    else
+      lapply(seq_row(x), function(i) setNames(as.list(x[i, ]), ns))
   } else if (is.data.frame(x)) {
     if (factors.as.char)
-      x = convertDataFrameCols(x, factors.as.char=TRUE)
+      x = convertDataFrameCols(x, factors.as.char = TRUE)
     rowLapply(x, function(row) setNames(as.list(row), ns))
   }
 }
--- inst/tests/test_convertRowsToList.R
+++ inst/tests/test_convertRowsToList.R
@@ -2,15 +2,19 @@ context("convertRowsToList")
 
 test_that("convertRowsToList", {
   expect_equal(
-    convertRowsToList(matrix(1:4, 2, byrow=TRUE)),
+    convertRowsToList(matrix(1:4, 2, byrow=TRUE), as.vector = TRUE),
+    list(c(1, 2), c(3, 4))
+  )
+  expect_equal(
+    convertRowsToList(matrix(1:4, 2, byrow=TRUE), as.vector = FALSE),
     list(list(1, 2), list(3, 4))
   )
   expect_equal(
-    convertRowsToList(setColNames(matrix(1:4, 2, byrow=TRUE), c("a", "b")), use.names=TRUE),
+    convertRowsToList(setColNames(matrix(1:4, 2, byrow=TRUE), c("a", "b")), use.names=TRUE, as.vector = FALSE),
     list(list(a=1, b=2), list(a=3, b=4))
   )
   expect_equal(
-    convertRowsToList(setColNames(matrix(1:4, 2, byrow=TRUE), c("a", "b")), use.names=FALSE),
+    convertRowsToList(setColNames(matrix(1:4, 2, byrow=TRUE), c("a", "b")), use.names=FALSE, as.vector = FALSE),
     list(list(1, 2), list(3, 4))
   )
   levs = c("a", "b")
