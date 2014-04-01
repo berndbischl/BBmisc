88f61682d009370ed828ff585f25976e38d7dd08
Bernd Bischl <bernd_bischl@gmx.net>
2014-04-01 13:22:26 +0700

    new fun: isScalar<Type>

--- inst/tests/test_isScalarValue.R
+++ inst/tests/test_isScalarValue.R
@@ -8,6 +8,20 @@ test_that("isScalarValue", {
   expect_true(isScalarValue(as.complex(1)))
   expect_true(isScalarValue(NA))
   
+  expect_true(isScalarNumeric(1))
+  expect_true(isScalarInteger(1L))
+  expect_true(isScalarCharacter("a"))
+  expect_true(isScalarFactor(factor("a")))
+  expect_true(isScalarComplex(as.complex(1)))
+  expect_true(isScalarLogical(NA))
+  
+  expect_false(isScalarComplex(1L))
+  expect_false(isScalarInteger(1))
+  expect_false(isScalarFactor("a"))
+  expect_false(isScalarCharacter(factor("a")))
+  expect_false(isScalarNumeric(as.complex(1)))
+  expect_false(isScalarInteger(NA))
+  
   expect_false(isScalarValue(NULL))
   expect_false(isScalarValue(iris))
   expect_false(isScalarValue(1:2))
