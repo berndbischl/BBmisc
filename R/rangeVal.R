bec2fc26118638dd2a8a7ee603b3b50fba0e8a33
Bernd Bischl <you@example.com>
2014-04-18 15:30:37 +0200

    rangeVal: add na.rm

--- R/rangeVal.R
+++ R/rangeVal.R
@@ -4,9 +4,18 @@
 #'
 #' @param x [\code{numeric}]\cr
 #'   The vector.
+#' @param na.rm [\code{logical(1)}]\cr
+#'   If \code{FALSE}, NA is returned if an NA is encountered in \code{x}.
+#'   If \code{TRUE}, NAs are disregarded.
+#'   Default is \code{FALSE}
 #' @return [\code{numeric(1)}].
 #' @export
-rangeVal = function(x) {
+rangeVal = function(x, na.rm = FALSE) {
   checkArg(x, "numeric", min.len = 1L, na.ok = TRUE)
-  diff(range(x))
+  checkArg(na.rm, "logical", len = 1L, na.ok = FALSE)
+  if (all(is.na(x)))
+    return(NA_real_)
+  diff(range(x, na.rm = na.rm))
 }
+
+
--- tests/testthat/test_rangeVal.R
+++ tests/testthat/test_rangeVal.R
@@ -5,4 +5,9 @@ test_that("rangeVal", {
   expect_equal(rangeVal(c(1, 5)), 4)
   expect_equal(rangeVal(1), 0)
   expect_equal(rangeVal(1:3), 2)
+  
+  # NAs
+  expect_equal(rangeVal(c(1, 2, NA)), NA_real_)
+  expect_equal(rangeVal(c(1, 2, NA), na.rm = TRUE), 1)
+  expect_equal(rangeVal(c(NA_real_), na.rm = TRUE), NA_real_)
 })
