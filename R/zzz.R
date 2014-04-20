7a708692407f03aab8c92fe86dbd0a8fea228d44
Bernd Bischl <you@example.com>
2014-04-20 15:05:31 +0200

    ProgressBar: we now output to stderr and have an option to select that stream

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 #' @import stats
 
 .onLoad = function(libname, pkgname) {
+  options(BBmisc.ProgressBar.stream = getOption("BBmisc.ProgressBar.stream", "stderr"))
   options(BBmisc.ProgressBar.style = getOption("BBmisc.ProgressBar.style", "text"))
   options(BBmisc.ProgressBar.width = getOption("BBmisc.ProgressBar.width", getOption("width")))
 }
