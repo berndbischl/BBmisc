88fe5c5ab604ccb907a958d25a44346a2b04b8b1
Jakob Bossek <info@jakobbossek.de>
2014-04-24 17:09:57 +0200

    Merging packages: hasAttributes

---
+++ R/hasAttributes.R
@@ -0,0 +1,13 @@
+#' Check if given object has certain attributes.
+#'
+#' @param obj [\code{mixed}]\cr
+#'   Arbitrary R object.
+#' @param attributeNames [\code{character}]\cr
+#'   Vector of strings, i.e., attribute names.
+#' @return [\code{logical(1)}]
+#'   \code{TRUE} if object \code{x} contains all attributes from \code{attributeNames}
+#'   and \code{FALSE} otherwise.
+#' @export
+hasAttributes = function(obj, attributeNames) {
+  return(is.subset(attributeNames, getAttributeNames(obj)))
+}
