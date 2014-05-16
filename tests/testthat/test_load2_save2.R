18508f5c4450272c0825741aa1f98955bb0ff3ad
Michel <michellang@gmail.com>
2014-05-16 15:26:48 +0200

    argument impute for load2

--- tests/testthat/test_load2_save2.R
+++ tests/testthat/test_load2_save2.R
@@ -12,7 +12,11 @@ test_that("load2", {
   ee = new.env()
   load2(fn, envir=ee)
   expect_equal(ee$x, 1)
-  
+
+  fn2 = file.path(fd, "xxx.RData")
+  expect_error(expect_warning(load2(fn2)))
+  expect_equal(load2(fn2, impute = NA), NA)
+
   save2(file=fn, x=1, y=2)
   z = list(x=1, y=2)
   expect_equal(load2(fn), z)
@@ -20,5 +24,3 @@ test_that("load2", {
   expect_equal(load2(fn, parts="x"), 1)
   expect_equal(load2(fn, parts="y"), 2)
 })
-
-
