7b1a6c5291b6e17d433437951f2e0169bac56cce
Bernd Bischl <you@example.com>
2014-05-16 19:08:56 +0200

    clean up setValue and use new roxygen2 style

--- R/setValue.R
+++ R/setValue.R
@@ -1,23 +1,28 @@
-#' Set a list element to a new Value. This wrapper supports setting elements to \code{NULL}.
+#' Set a list element to a new value.
+#'
+#' This wrapper supports setting elements to \code{NULL}.
 #'
 #' @param obj [\code{list}]\cr
 #' @param index [\code{character} | \code{integer}]\cr
-#'   Index where to put the new Value(s).
-#' @param newVal [mixed]
-#'   Will be wrapped in a new list element. Has to be a list if \code{index} is a vector.
+#'   Index or indices where to insert the new values.
+#' @param newval [any]\cr
+#'   Inserted elements(s). 
+#'   Has to be a list if \code{index} is a vector.
 #' @return [\code{list}]
 #' @export
-setValue = function(obj, index, newVal){
+setValue = function(obj, index, newval) {
+  checkArg(obj, "list")
   index = convertIntegers(index)
-  checkArg(obj, cl="list")
-  if(length(index)>1)
-    checkArg(newVal, cl="list")
-  if(!is.null(newVal))
-    checkArg(index, cl=c("integer","character"), len=length(newVal))
-  if(!is.list(newVal))
-    newVal = list(newVal)
-  names(newVal) = index
-  obj[index] = newVal
-  obj
+  checkArg(index, cl = c("integer","character"))
+  if (length(index) == 1L) {
+    if (is.null(newval))
+      obj[index] = list(NULL)
+    else
+      obj[index] = newval
+  } else {
+    checkArg(newval, cl = "list", len = length(index))
+    obj[index] = newval
+  }
+  return(obj)
 }
 
