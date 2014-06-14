aa561368585c3bb4143699769e52b0aee6a0e3aa
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-14 17:18:02 +0200

    doc: clean up

--- R/convertToShortString.R
+++ R/convertToShortString.R
@@ -1,16 +1,17 @@
-#' Converts any R object to a descriptive string so it can be used in messages.
+#' @title Converts any R object to a descriptive string so it can be used in messages.
 #'
+#' @description
 #' Atomics: If of length 0 or 1, they are basically printed as they are.
-#' Numerics are formated with \code{num.format}. 
-#' If of length greater than 1, they are collpsed witd \dQuote{,} and clipped.
+#' Numerics are formated with \code{num.format}.
+#' If of length greater than 1, they are collapsed witd \dQuote{,} and clipped.
 #' so they do not become excessively long.
 #'
-#' All others: Currently, only their class is simply printed 
+#' All others: Currently, only their class is simply printed
 #' like \dQuote{<data.frame>}.
 #'
 #' Lists: The mechanism above is applied (non-recursively) to their elements.
 #' The result looks like this:
-#' \dQuote{a=1, <unamed>=2, b=<data.frame>, c=<list>}.
+#' \dQuote{a = 1, <unamed> = 2, b = <data.frame>, c = <list>}.
 #'
 #' @param x [any]\cr
 #'   The object.
@@ -23,8 +24,8 @@
 #' @return [\code{character(1)}].
 #' @export
 #' @examples
-#' convertToShortString(list(a=1, b=NULL, "foo", c=1:10))
-convertToShortString = function(x, num.format="%.4g", clip.len=15L) {
+#' convertToShortString(list(a = 1, b = NULL, "foo", c = 1:10))
+convertToShortString = function(x, num.format = "%.4g", clip.len = 15L) {
 
   # convert non-list object to string
   convObj = function(x) {
@@ -32,29 +33,27 @@ convertToShortString = function(x, num.format="%.4g", clip.len=15L) {
       return("NULL")
     if (is.atomic(x)) {
       if (length(x) == 0L) {
-        sprintf("%s(0)", class(x)[1L])
+        sprintf("%s(0)", getClass(x))
       } else if (length(x) == 1L) {
         if (is.double(x))
           sprintf(num.format, x)
         else
-          paste(x, collapse=",")
+          collapse(x)
       } else {
         clipString(collapse(sapply(x, convertToShortString), ","), clip.len)
-        # capture.output(str(x, nchar.max=10L, give.attr=FALSE, 
-          # give.head=FALSE, indent.str=""))
       }
     } else {
-      paste("<", class(x)[1L], ">", sep="")
+      paste("<", getClass(x), ">", sep = "")
     }
   }
 
   # handle only lists and not any derived data types
-  if (class(x)[1L] == "list") {
+  if (getClass(x) == "list") {
     if (length(x) == 0L)
       return("")
     ns = names2(x, missing.val = "<unnamed>")
     ss = lapply(x, convObj)
-    collapse(paste(ns, "=", ss, sep=""), ", ")
+    collapse(paste(ns, "=", ss, sep = ""), ", ")
   } else {
     convObj(x)
   }
