abf70f813b7146e161ee487e183642f0db619674
Michel <michellang@gmail.com>
2014-09-15 15:23:18 +0200

    prettyPrint cleanups

--- R/prettyPrint.R
+++ R/prettyPrint.R
@@ -10,14 +10,14 @@ prettyPrint = function(x, indent.long = NULL, prefix = "", sep = ": ", key.align
   nc.prefix = nchar(prefix)
   nc.sep = nchar(sep)
   nc.left = nc.prefix + nc.key + nc.sep
-  nc.right = width - nc.left
-  nc.indent = ifelse(is.null(indent.long), nc.key + nc.sep, asCount(indent.long))
-  fmt = ifelse(key.align == "left", "%s%%%is%s%%s", "%s%%-%is%s%%s")
+  nc.right = max(width - nc.left, 0L)
+  nc.indent = ifelse(is.null(indent.long), nc.left, asCount(indent.long))
+  fmt = ifelse(key.align == "left", "%s%%-%is%s%%s", "%s%%%is%s%%s")
   fmt = sprintf(fmt, prefix, nc.key, sep)
 
   width.before = getOption("width", 80L)
   on.exit(options(width = width.before))
-  options(width = width - nc.left)
+  options(width = min(max(width - nc.indent, 10L), 10000L))
 
   for (i in seq_along(x)) {
     val = x[[i]]
@@ -31,10 +31,15 @@ prettyPrint = function(x, indent.long = NULL, prefix = "", sep = ": ", key.align
       }
       val = collapse(val, "\n")
     } else {
-      val = paste0("\n", collapse(paste0(prefix, strrepeat(" ", nc.indent), capture.output(print(val))), "\n"))
+      val = paste0(prefix, strrepeat(" ", nc.indent), capture.output(print(val)))
+      if (nc.indent == nc.left) {
+        val[1L] = sub("^[[:space:]]+", "", val[1L])
+        val = collapse(val, "\n")
+      } else {
+        val = paste0("\n", collapse(val, "\n"))
+      }
     }
     cat(sprintf(fmt, keys[i], val), "\n")
-
   }
   invisible(TRUE)
 }
