26fad82c6ab6b786ca272e3c8ae79fce674327ee
Michel <michellang@gmail.com>
2014-10-14 12:11:20 +0200

    more tests for do.call2

--- tests/testthat/test_do.call2.R
+++ tests/testthat/test_do.call2.R
@@ -6,10 +6,16 @@ test_that("do.call2", {
   expect_equal(do.call2("f", .args=list(a=1, b=2)), list(a=1, b=2))
   expect_equal(do.call2("f", a=1, .args=list(b=2)), list(a=1, b=2))
 
-  oos = iris
-  expect_equal(do.call2("f", oos), list(oos))
+  df = iris
+  expect_equal(do.call2("f", df), list(df))
+  expect_equal(do.call2("f", .args = list(df)), list(df))
 
   f = function(x, data) data[[x]]
-  do.call2("f", "Species", data=iris)
-  do.call2("f", data=iris, .args=list(x = "Species"))
+  expect_equal(do.call2("f", "Species", data=iris), iris$Species)
+  expect_equal(do.call2("f", "Species", iris), iris$Species)
+  expect_equal(do.call2("f", data = iris, "Species"), iris$Species)
+  expect_equal(do.call2("f", "Species", .args = list(data = iris)), iris$Species)
+  expect_equal(do.call2("f", data = iris, .args = list(x = "Species")), iris$Species)
+
+  expect_error(do.call2(mean, 1:10), "string")
 })
