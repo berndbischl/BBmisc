8a69dfe83e5459d3a352c7623b6f971a76815154
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-27 17:52:54 +0100

    coalesce: add test for functions. 2nd unit test breaks. see issue 48.

--- tests/testthat/test_coalesce.R
+++ tests/testthat/test_coalesce.R
@@ -5,6 +5,13 @@ test_that("coalesce", {
   expect_identical(coalesce(1, NULL), 1)
   expect_identical(coalesce(NULL, 1), 1)
   f = function(a, b, c) coalesce(a,b,c)
-  expect_identical(f(b=NULL, c=1), 1)
+  expect_identical(f(b = NULL, c = 1), 1)
 })
+
+test_that("coalesce works with functions", {
+  ff = function(x) 123
+  expect_identical(coalesce(NULL, ff), ff)
+  expect_identical(coalesce(NULL, min), min)
+})
+
 
