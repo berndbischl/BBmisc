fa17e100cb82c6fc4dc71e52bf20b0d2da0db042
Bernd Bischl <bernd_bischl@gmx.net>
2014-12-12 18:49:11 +0100

    use requireNS in requirePackages

--- R/requirePackages.R
+++ R/requirePackages.R
@@ -1,4 +1,7 @@
-#' Require some packages.
+#' @title Require some packages.
+#'
+#' @description
+#' Packages are loaded either via \code{\link{requireNamespace}} or \code{\link{require}}.
 #'
 #' If some packages could not be loaded and \code{stop} is \code{TRUE}
 #' the following exception is thrown:
@@ -15,19 +18,28 @@
 #'   Should an exception be thrown for missing packages?
 #'   Default is \code{TRUE}.
 #' @param suppress.warnings [\code{logical(1)}]\cr
-#'   Should warnings be suppressed in the calls to \code{\link{require}}?
+#'   Should warnings be suppressed while requiring?
 #'   Default is \code{FALSE}.
 #' @param suppress.startup [\code{logical(1)}]\cr
 #'   Should package startup messages be suppressed?
 #'   Default is \code{TRUE}.
+#' @param namespace.only [\code{logical(1)}]\cr
+#'   Use \code{\link{requireNamespace}} instead of \code{\link{require}} to require the package?
+#'   The former does not attach the package to the global namespace.
+#'   This is probably what you want, if you call this function in a package to internally
+#'   load a suggested package, and you normally do not want to pollute the global namespace
+#'   in such a case.
+#'   Default is \code{TRUE}.
 #' @param ... [any]\cr
-#'   Passed on to \code{\link{require}}.
+#'   Passed on to \code{\link{requireNamespace}} or \code{\link{require}}.
 #' @return [\code{logical}]. Named logical vector describing which packages could be loaded.
 #'   Same length as \code{packs}.
 #' @export
 #' @examples
 #' requirePackages(c("BBmisc", "base"), why = "BBmisc example")
-requirePackages = function(packs, why = NULL, stop = TRUE, suppress.warnings = FALSE, suppress.startup = TRUE, ...) {
+requirePackages = function(packs, why = NULL, stop = TRUE, suppress.warnings = FALSE,
+  suppress.startup = TRUE, namespace.only = TRUE, ...) {
+
   getSuppressor = function(suppress.warnings, suppress.startup) {
     if (suppress.warnings) {
       if (suppress.startup)
@@ -42,9 +54,10 @@ requirePackages = function(packs, why = NULL, stop = TRUE, suppress.warnings = F
   args = list(...)
   args$character.only = TRUE
   suppressor = getSuppressor(suppress.warnings, suppress.startup)
+  reqfun = if (namespace.only) requireNamespace else require
   packs.ok = sapply(packs, function(x) {
     args$package = x
-    suppressor(do.call(require, args))
+    suppressor(do.call(reqfun, args))
   })
   if(stop && !all(packs.ok)) {
     ps = collapse(packs[!packs.ok])
