347ceece7ca177dd06b37a798344c2704e9ed6f0
Jakob Bossek <info@jakobbossek.de>
2016-08-16 11:09:07 +0200

    Add helper function getBestIndex: simple wrapper for get{Min,Max}Index

--- R/getMaxIndex.R
+++ R/getMaxIndex.R
@@ -1,7 +1,11 @@
-#' Return index of maximal/minimal element in numerical vector.
+#' Return index of maximal/minimal/best element in numerical vector.
 #'
 #' If \code{x} is empty or only contains NAs which are to be removed,
 #' -1 is returned.
+#'
+#' @note
+#' Function \code{getBestIndex} is a simple wrapper for \code{getMinIndex} or
+#' \code{getMaxIndex} respectively depending on the argument \code{minimize}.
 #'
 #' @param x [\code{numeric}]\cr
 #'   Input vector.
@@ -13,6 +17,11 @@
 #'   If \code{FALSE}, NA is returned if an NA is encountered in \code{x}.
 #'   If \code{TRUE}, NAs are disregarded.
 #'   Default is \code{FALSE}
+#' @param minimize [\code{logical(1)}]\cr
+#'   Minimal element is considered best?
+#'   Default is \code{TRUE}.
+#' @param ... [any]\cr
+#'   Further arguments passed down to the delegate.
 #' @return [\code{integer(1)}].
 #' @export
 #' @useDynLib BBmisc c_getMaxIndex
@@ -28,4 +37,13 @@ getMaxIndex = function(x, ties.method = "random", na.rm = FALSE) {
 #' @rdname getMaxIndex
 getMinIndex = function(x, ties.method = "random", na.rm = FALSE) {
   getMaxIndex(-as.numeric(x), ties.method, na.rm)
+}
+
+
+#' @export
+#' @rdname getMaxIndex
+getBestIndex = function(x, minimize = TRUE, ...) {
+  assertFlag(minimize)
+  getIndex = if (minimize) getMinIndex else getMaxIndex
+  getIndex(x, ...)
 }
