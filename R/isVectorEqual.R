e3a711abe4c44b9890f4e4acfdef9a494c8c9a6e
Jakob Richter <code@jakob-r.de>
2016-09-19 16:20:48 +0200

    Add isVectorEqual function (Fixes #30)

--- R/isVectorEqual.R
+++ R/isVectorEqual.R
@@ -7,15 +7,17 @@
 #' @param y [\code{vector(n)}]\cr
 #'   Vector of the same length as \code{x}.
 #' @return [\code{logical(n)}]. For each value in \code{x}: Is it the same as in \code{y}?
-#' @usage x \%equals\% y
-#' @rdname equals
 #' @export
 #' @examples
 #' x = c(1, 2, NA)
 #' y = c(1L, 2L, NA)
-#' x %equals% y
-`%equals%` = function(x, y) {
-  checkArg(x, "vector", len = length(y))
+#' isVectorEqual(x,y)
+isVectorEqual = function(x, y) {
+  assertVector(x)
+  assertVector(y)
+  if (length(x) != length(y)) {
+    stopf("Vecors are not of the same length! x: %i, y: %i", length(x), length(y))
+  }
   res = (x == y) | (is.na(x) & is.na(y))
   replace(res, is.na(res), FALSE)
 }
