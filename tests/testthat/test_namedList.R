3709ffc042bf0d8a38dc55d4710917e48993606e
Jakob Richter <code@jakob-r.de>
2016-11-10 17:15:07 +0100

    expect empty named list and not only list

--- tests/testthat/test_namedList.R
+++ tests/testthat/test_namedList.R
@@ -1,7 +1,7 @@
 context("namedList")
 
 test_that("namedList", {
-  expect_equal(namedList(), vector("list", 0))
+  expect_equal(namedList(), setNames(vector("list", 0), nm = character()))
   expect_equal(namedList("a"), list(a=NULL))
   expect_equal(namedList(c("a", "b")), list(a=NULL, b=NULL))
   expect_equal(namedList(c("a", "b"), 1), list(a=1, b=1))
