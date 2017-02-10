9d30601682c2483ca94ceac225263d49334cb29e
Bernd Bischl <bernd_bischl@gmx.net>
2017-02-10 01:07:54 +0100

    coalesce: improve docs

--- R/coalesce.R
+++ R/coalesce.R
@@ -1,7 +1,11 @@
-#' Returns first non-missing, non-null argument.
+#' @title Returns first non-missing, non-null argument.
 #'
-#' Returns first non-missing, non-null argument, otherwise
-#' \code{NULL}.
+#' @description
+#' Returns first non-missing, non-null argument, otherwise \code{NULL}.
+#'
+#' We have to perform some pretty weird \code{\link{tryCatch}} stuff internally,
+#' so you should better not pass complex function calls into the arguments that can throw exceptions,
+#' as these will be completely muffled, and return \code{NULL} in the end.
 #'
 #' @param ... [any]\cr
 #'   Arguments.
@@ -15,15 +19,13 @@
 coalesce = function(...) {
   dots = match.call(expand.dots = FALSE)$...
   for (arg in dots) {
-    is_missing = if (is.symbol(arg)) {
-      eval(substitute(missing(symbol), list(symbol = arg)),
-           envir = parent.frame())
+    ismissing = if (is.symbol(arg)) {
+      eval(substitute(missing(symbol), list(symbol = arg)), envir = parent.frame())
     } else {
       FALSE
     }
-    if (!is_missing) {
-      value = tryCatch(eval(arg, envir = parent.frame()),
-                       error = function(...) NULL)
+    if (!ismissing) {
+      value = tryCatch(eval(arg, envir = parent.frame()), error = function(...) NULL)
       if (!is.null(value)) {
         return(value)
       }
