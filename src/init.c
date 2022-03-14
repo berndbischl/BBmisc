fa9de4e6e091d313a315b52ccfc54eb1f30966a3
Michel Lang <michellang@gmail.com>
2022-03-14 09:23:38 +0100

    New release (#91)

--- src/init.c
+++ src/init.c
@@ -7,16 +7,12 @@
 extern SEXP c_getMaxIndex(SEXP, SEXP, SEXP, SEXP);
 extern SEXP c_getMaxIndexOfCols(SEXP, SEXP, SEXP, SEXP);
 extern SEXP c_getMaxIndexOfRows(SEXP, SEXP, SEXP, SEXP);
-extern SEXP c_which_first(SEXP, SEXP);
-extern SEXP c_which_last(SEXP, SEXP);
 extern SEXP itostr(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"c_getMaxIndex",       (DL_FUNC) &c_getMaxIndex,       4},
     {"c_getMaxIndexOfCols", (DL_FUNC) &c_getMaxIndexOfCols, 4},
     {"c_getMaxIndexOfRows", (DL_FUNC) &c_getMaxIndexOfRows, 4},
-    {"c_which_first",       (DL_FUNC) &c_which_first,       2},
-    {"c_which_last",        (DL_FUNC) &c_which_last,        2},
     {"itostr",              (DL_FUNC) &itostr,              2},
     {NULL, NULL, 0}
 };
